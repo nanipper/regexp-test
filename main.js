d307dc00d2539968e057c4ad0a9ee968395a5b05
nanipper <nanipper@naver.com>
2022-06-22 16:42:46 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,39 @@
+const str = `
+010-3670-1777
+nanipper@naver.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd 
+hxyp`
+
+
+
+console.log(
+    str.match(/h..p/g),
+    str.match(/h..p/g)
+
+)
+
+
+
+
+
+
+// const regexp = new RegExp('the', 'gi')
+
+// const regexp = /the/gi
+// console.log(str.match(regexp))
+
+// const regexp = /fox/gi
+// console.log(regexp.test(str))
+
+// const regexp = /fox/gi
+// console.log(str.replace(regexp, 'AAA'))
+
+// console.log(str)
+
+
+// const regexp = /the/
+// console.log(str.replace(regexp, 'AAA'))
+
+// console.log(str)
